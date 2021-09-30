8b771c7eb6d08461ae4dfb813ebe50e8be8f949a
matheusrpd <matheusrpd@gmail.com>
2021-09-30 17:30:46 -0300

    Adicionando miragejs para emular api.

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    async function getTransactions() {
+      const response = await api.get('/transactions');
+      console.log(response.data);
+    }
+
+    getTransactions();
+  }, []);
+
   return (
     <Container>
       <table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          type: 'deposit',
+          title: 'Desenvolvimento de website',
+          category: 'Desenvolvimento',
+          amount: 12000,
+          createdAt: new Date()
+        }
+      ]
+    });
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
